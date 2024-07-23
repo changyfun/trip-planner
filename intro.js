f3dcca2f2ddd8e45e7e0434e673cd67dab49f7bf
changyfun <2455036527@qq.com>
2024-07-23 08:41:25 +0800

    1235

--- intro.js
+++ intro.js
@@ -10,11 +10,7 @@
         for (var d; a.length && (d = a.shift()); )
             a.length || void 0 === b ? c = c[d] && c[d] !== Object.prototype[d] ? c[d] : c[d] = {} : c[d] = b
     };
-    function q() {
-        for (var a = r, b = {}, c = 0; c < a.length; ++c)
-            b[a[c]] = c;
-        return b
-    }
+  
     function u() {
         var a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         a += a.toLowerCase() + "0123456789-_";
